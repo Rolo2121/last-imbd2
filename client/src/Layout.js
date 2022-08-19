bae0c4e02eaf65f026a7609375e7be2f15f5ed04
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-19 13:00:09 -0500

    commit

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
 import "./index.css";
-import {
-  PageHeader,
-  Breadcrumb,
-  Layout,
-  Menu,
-  Col,
-  Row,
-  TimePicker,
-  Form,
-  Input,
-  Button,
-  Space,
-  Card
-} from "antd";
+import { PageHeader, Breadcrumb, Layout, Menu, Col, Row, TimePicker, Form, Input, Button, Space, Card } from "antd";
+import MovieCard from "./MovieCard";
 
 const { Header, Content, Footer } = Layout;
-const temp = [1, 2, 3, 4];
-const App = () => {
+const AppLayout = () => {
   const [form] = Form.useForm();
   const onFormLayoutChange = (fdata) => {};
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const updateMovies = async () => {
+      const response = await fetch("/api/movie");
+      const data = await response.json();
+      setMovies(data);
+    };
+    updateMovies();
+  }, []);
   return (
     <Layout className="layout">
       <Header>
@@ -44,12 +40,7 @@ const App = () => {
         }}
       >
         <div className="site-layout-content">
-          <Form
-            layout="vertical"
-            form={form}
-            onValuesChange={onFormLayoutChange}
-            className="margin-top-2"
-          >
+          <Form layout="vertical" form={form} onValuesChange={onFormLayoutChange} className="margin-top-2">
             <Form.Item label="Enter Movie Name:">
               <Input placeholder="Find a Movie" />
             </Form.Item>
@@ -59,27 +50,13 @@ const App = () => {
           </Form>
 
           <Row gutter={[16, 16]}>
-            {temp.map((t, i) => (
-              <Col xs={24} sm={12} md={8} lg={6} xlg={4}>
-                <Card
-                  hoverable
-                  style={{
-                    width: '100%',
-                  }}
-                  cover={
-                    <img
-                      alt="example"
-                      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    />
-                  }
-                >
-                  <Card.Meta
-                    title="Europe Street beat"
-
-                  />
-                </Card>
-              </Col>
-            ))}
+            {Array(5)
+              .fill(movies[0])
+              .map((movie) => (
+                <Col xs={24} sm={12} md={8} lg={6} xlg={4}>
+                  <MovieCard movie={movie} />
+                </Col>
+              ))}
           </Row>
         </div>
       </Content>
@@ -94,4 +71,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default AppLayout;
