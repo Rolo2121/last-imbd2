92850ac23463a489b94ea0ca2cd5ed83d2550dc2
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-08-19 22:49:22 -0500

    this is jazelles work i'm pulling

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Layout from './pages/Layout';
-import Login from './pages/Login'
-import{Routes, Route, BrowserRouter} from 'react-router-dom'
-import Movie from './pages/Movie'
+import Login from './pages/Login';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Movie from './pages/Movie';
 function App() {
-  return ( <BrowserRouter>
-    <div className="App">
-      <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/' element={ <Layout/> }/>
-        <Route path='/movie/:id' element={ <Movie/> }/>
-      </Routes>
-
-    </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/" element={<Layout />} />
+					<Route path="/movie/:id" element={<Movie />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
