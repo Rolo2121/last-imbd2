9e5a749b695b30cd7679fc66488cc6a29834c48e
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-24 16:42:52 -0500

    added create account logic

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -1,41 +1,35 @@
-import React, { useState } from 'react';
-import { PlusOutlined } from '@ant-design/icons';
-import Nav from './Nav'
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import Nav from "./Nav";
 import "../index.css";
-import {
-  Form,
-  Input,
-  Button,
-  Radio,
-  Select,
-  Cascader,
-  DatePicker,
-  InputNumber,
-  TreeSelect,
-  Switch,
-  Checkbox,
-  Upload,
-} from 'antd';
+import { Form, Input, Button, Radio, Select, Cascader, DatePicker, InputNumber, TreeSelect, Switch, Checkbox, Upload } from "antd";
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const App = () => {
   const [form] = Form.useForm();
-  const [fileList, setFileList] = useState([])
+  const [fileList, setFileList] = useState([]);
   const [componentDisabled, setComponentDisabled] = useState(false);
-  const onFinish = (values) => {
-    console.log(values);
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/user/signup", values);
+      onLogin();
+      navigate("/watchlist");
+    } catch (error) {
+      console.error(error.response);
+    }
   };
   const onFormLayoutChange = ({ disabled }) => {
-   /* setComponentDisabled(disabled);
-  };*/}
+    /* setComponentDisabled(disabled);
+  };*/
+  };
 
   return (
     <>
-<Nav></Nav>
+      <Nav></Nav>
       <Form
-      form={form}
-      onFinish={onFinish}
+        form={form}
+        onFinish={onFinish}
         labelCol={{
           span: 4,
         }}
@@ -46,28 +40,28 @@ const App = () => {
         onValuesChange={onFormLayoutChange}
         disabled={componentDisabled}
       >
-
-
         <Form.Item label="Email" name="email">
           <Input />
         </Form.Item>
-        <Form.Item label="Username"name="username">
+        <Form.Item label="Username" name="username">
           <Input />
         </Form.Item>
         <Form.Item label="Password" name="password">
           <Input.Password />
         </Form.Item>
 
-
         <Form.Item label="Profile Pic" valuePropName="fileList">
-          <Upload fileList={fileList}
-          onChange={({fileList:fl})=>{
-            setFileList(fl)
-          }}
-          beforeUpload={()=>{
-            return false
-          }}
-          action="/upload.do" listType="picture-card">
+          <Upload
+            fileList={fileList}
+            onChange={({ fileList: fl }) => {
+              setFileList(fl);
+            }}
+            beforeUpload={() => {
+              return false;
+            }}
+            action="/upload.do"
+            listType="picture-card"
+          >
             <div>
               <PlusOutlined />
               <div
