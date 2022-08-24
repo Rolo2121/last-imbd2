677888099649fc49111b0d85925eb71b83bedd54
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-08-23 19:17:14 -0500

    watchlist base code

--- client/src/pages/WatchList.js
+++ client/src/pages/WatchList.js
@@ -1,1 +1,22 @@
-export default function WatchList() {}
+// import React, { useContext } from 'react';
+// import MovieCard from '../MovieCard';
+
+// export default function WatchList() {
+// 	const { watchlist } = useContext;
+
+// 	return (
+// 		<div className="movie-page">
+// 			<div classNane="container">
+// 				<div className="header">
+// 					<div className="heading">Watch List</div>
+// 				</div>
+// 				{watchlist.length}
+// 				<div className="movie-grid">
+// 					{watchlist.map((movie) => (
+// 						<MovieCard movie={movie} type="watchlist" />
+// 					))}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
