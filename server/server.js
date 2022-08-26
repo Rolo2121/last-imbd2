e5917320129eb1a908b810a731a7eaa98d253d9f
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-26 07:21:18 -0500

    User.js and server.js

--- server/server.js
+++ server/server.js
@@ -1,40 +1,72 @@
 //need this
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const path = require("path");
-const config = require("./config/key");
-const mongoose = require("mongoose");
-const session = require("express-session");
+const express = require("express");
+const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
-const connect = mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(cookieParser());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-app.use("/api/user", require("./routes/users"));
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
+// const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const mongoose = require("mongoose");
+// const session = require("express-session");
+
+// const connect = mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected..."))
+//   .catch((err) => console.log(err));
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
+// app.use(cookieParser());
+
+// app.use("/api/user", require("./routes/users"));
 // app.use("/api/comment", require("./routes/comment"));
 // app.use("/api/like", require("./routes/like"));
 // app.use("/api/favorite", require("./routes/favorite"));
-app.use("/api/movie", require("./routes/movie"));
+// app.use("/api/movie", require("./routes/movie"));
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
-const port = process.env.PORT || 5001;
+// const port = process.env.PORT || 5001;
 
-app.listen(port, () => {
-  console.log(`Sever Running at ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Sever Running at ${port}`);
+// });
