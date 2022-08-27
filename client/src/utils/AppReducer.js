762bde02e8933d242591259172b65526374d4798
Jacob <jakebauer21@gmail.com>
2022-08-27 14:03:35 -0500

    added reducers and actions

--- client/src/utils/AppReducer.js
+++ client/src/utils/AppReducer.js
@@ -1,6 +1,19 @@
-export default (state, action) => {
+import { useReducer } from 'react';
+
+import { ADD_TO_WATCHLIST } from './actions';
+
+export const reducer = (state, action) => {
 	switch (action.type) {
+		case ADD_TO_WATCHLIST:
+			return {
+				...state,
+				movies: action.movies,
+			};
 		default:
-			return state;
+			return state ? state : '';
 	}
 };
+
+export function useMovieReduce(initialState) {
+	return useReducer(reducer, initialState);
+}
