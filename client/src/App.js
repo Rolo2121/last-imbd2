91715f0bf9552dc5cf2dc2b292fc8685a210919b
Jacob <jakebauer21@gmail.com>
2022-08-27 10:12:44 -0500

    added actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,87 @@
-import "./App.css";
-import Layout from "./pages/Layout";
-import CreateAccount from "./pages/CreateAccount";
-import Login from "./pages/Login";
-import Watchlist from "./pages/WatchList";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import './App.css';
+import Layout from './pages/Layout';
+import CreateAccount from './pages/CreateAccount';
+import Login from './pages/Login';
+import Watchlist from './pages/WatchList';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import Movie from "./pages/Movie";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Movie from './pages/Movie';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useQuery, gql, useMutation } from '@apollo/client';
-import { GET_WATCHLIST } from "./utils/queries";
-import { UPDATE_WATCHLIST } from "./utils/mutations";
+import { GET_WATCHLIST } from './utils/queries';
+import { UPDATE_WATCHLIST } from './utils/mutations';
+import { GET_MOVIES } from './utils/actions';
 
 // import { ifError } from 'assert';
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const {data: movies} = useQuery(GET_MOVIES)
-  const {data: watchlist} = useQuery(GET_WATCHLIST)
-  const [mutateFunction, {data: updatedWatchlist}] = useMutation(UPDATE_WATCHLIST)
-  const [movie, setMovie] = useState(null);
- 
+	const [loggedIn, setLoggedIn] = useState(false);
+	const { data: movies } = useQuery(GET_MOVIES);
+	const { data: watchlist } = useQuery(GET_WATCHLIST);
+	const [mutateFunction, { data: updatedWatchlist }] =
+		useMutation(UPDATE_WATCHLIST);
+	const [movie, setMovie] = useState(null);
 
-  function login() {
-    setLoggedIn(true);
-  }
-  function logout() {
-    setLoggedIn(false);
-  }
+	function login() {
+		setLoggedIn(true);
+	}
+	function logout() {
+		setLoggedIn(false);
+	}
 
-  function removeFromWatchlist(id) {
-    setWatchlist(watchlist.filter((entry) => entry._id !== id));
-  }
-  function addToWatchlist(id) {
-    const foundMovie = movies.find((movie) => movie._id === id);
-    if (foundMovie) {
-      setWatchlist([...watchlist, foundMovie]);
-      mutateFunction({variables: {movieId: foundMovie._id}})
-    }
-  }
+	function removeFromWatchlist(id) {
+		setWatchlist(watchlist.filter((entry) => entry._id !== id));
+	}
+	function addToWatchlist(id) {
+		const foundMovie = movies.find((movie) => movie._id === id);
+		if (foundMovie) {
+			setWatchlist([...watchlist, foundMovie]);
+			mutateFunction({ variables: { movieId: foundMovie._id } });
+		}
+	}
 
-  useEffect(() => {
-    async function checkLoggedIn() {
-      try {
-        const response = await axios.get("/api/user/ping");
-        login();
-      } catch (error) {}
-    }
-    checkLoggedIn();
-  }, []);
+	useEffect(() => {
+		async function checkLoggedIn() {
+			try {
+				const response = await axios.get('/api/user/ping');
+				login();
+			} catch (error) {}
+		}
+		checkLoggedIn();
+	}, []);
 
- 
-
-  
-
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/createaccount" element={<CreateAccount onLogin={login} />} />
-          <Route path="/login" element={<Login onLogin={login} />} />
-          <Route
-            path="/"
-            element={
-              <Layout
-                setMovie={(movie) => {
-                  setMovie(movie);
-                }}
-                movies={movies}
-                onAdd={addToWatchlist}
-              />
-            }
-          />
-          <Route path="/movie" element={<Movie movie={movie} />} />
-          <Route path="/watchlist" element={<Watchlist watchlist={watchlist} onRemove={removeFromWatchlist} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route
+						path="/createaccount"
+						element={<CreateAccount onLogin={login} />}
+					/>
+					<Route path="/login" element={<Login onLogin={login} />} />
+					<Route
+						path="/"
+						element={
+							<Layout
+								setMovie={(movie) => {
+									setMovie(movie);
+								}}
+								movies={movies}
+								onAdd={addToWatchlist}
+							/>
+						}
+					/>
+					<Route path="/movie" element={<Movie movie={movie} />} />
+					<Route
+						path="/watchlist"
+						element={
+							<Watchlist watchlist={watchlist} onRemove={removeFromWatchlist} />
+						}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- client/src/utils/AppReducer.js
+++ client/src/utils/AppReducer.js
@@ -1,6 +1,24 @@
-export default (state, action) => {
+import { useReducer } from 'react';
+
+import {
+	GET_MOVIES,
+	GET_WATCHLIST,
+	ADD_TO_WATCHLIST,
+	REMOVE_FROM_WATCHLIST,
+} from './actions';
+
+export const reducer = (state, action) => {
 	switch (action.type) {
+		case ADD_TO_WATCHLIST:
+			return {
+				...state,
+				movies: [...state.movies, action.movie],
+			};
 		default:
-			return state;
+			return state ? state : '';
 	}
 };
+
+export function useMovieReducer(initialState) {
+	return useReducer(reducer, initialState);
+}
